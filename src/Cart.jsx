cc54b4b5d169887994e01f65297140ed37670524
Ninad Kadam <ninad_kadam@ildc-inlp.idealake.com>
2026-02-16 14:59:43 +0530

    cart code updated as cart value is 0

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -9,32 +9,37 @@ const Cart = () => {
   return (
     <>
       <Container>
-        <div className="d-flex justify-content-between border-bottom mb-2 px-3">
-          <h4>Cart Items</h4>
-          <h6 className="float-end">{cartItems.length} items</h6>
-        </div>
         {
           cartItems && cartItems.map((item) => (
-            <Row className="align-items-center border-bottom py-3" key={item.id}>
-              <Col md={1}>
-                <img src={item.thumbnail} alt={item.title} width="100" />
-              </Col>
-              <Col md={9}>
-                <h5>{item.title}</h5>
-                <p>{item.brand}</p>
-              </Col>
-              <Col md={2} className="text-end">
-                <h6>Qty: {item.quantity}</h6>
-                <h6 className="original-price">Price: {item.price}</h6>
-                <h6 className="total-price">Total: {(item.price * item.quantity).toFixed(2)}</h6>
-                <Button onClick={() => dispatch(removeCart(item))} variant="outline-danger">Remove</Button>
-              </Col>
-            </Row>
+            <>
+              <div className="d-flex justify-content-between border-bottom mb-2 px-3">
+                <h4>Cart Items</h4>
+                <h6 className="float-end">{cartItems.length} items</h6>
+              </div>
+              <Row className="align-items-center border-bottom py-3" key={item.id}>
+                <Col md={1}>
+                  <img src={item.thumbnail} alt={item.title} width="100" />
+                </Col>
+                <Col md={9}>
+                  <h5>{item.title}</h5>
+                  <p>{item.brand}</p>
+                </Col>
+                <Col md={2} className="text-end">
+                  <h6>Qty: {item.quantity}</h6>
+                  <h6 className="original-price">Price: {item.price}</h6>
+                  <h6 className="total-price">Total: {(item.price * item.quantity).toFixed(2)}</h6>
+                  <Button onClick={() => dispatch(removeCart(item))} variant="outline-danger">Remove</Button>
+                </Col>
+              </Row>
+              <div className="text-end pt-3">
+                <h4>Total:{" "}{cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0).toFixed(2)}</h4>
+              </div>
+            </>
           ))
         }
-        <div className="text-end pt-3">
-          <h4>Total:{" "}{cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0).toFixed(2)}</h4>
-        </div>
+        {
+          cartItems.length === 0 && <h1 className="text-center mt-5">Your Cart is Empty</h1>
+        }
       </Container>
     </>
   );
